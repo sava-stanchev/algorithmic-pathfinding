6f75e02eb62f4899c70b069c2165126109f46624
sava-stanchev <s.stanchev94@gmail.com>
2025-04-22 21:29:22 +0300

    refactor and clean up dijkstra algo logic

--- src/Algorithms/Dijkstra.ts
+++ src/Algorithms/Dijkstra.ts
@@ -1,46 +1,47 @@
 import { getUnvisitedNeighbors } from "../HelperFuncs/AlgorithmHelpers";
 import { NodeType } from "../Visualizer/SingleNode/SingleNode";
 
-const getAllNodes = (grid: NodeType[][]) => {
-  const nodes = [];
-  for (const col of grid) {
-    for (const node of col) {
-      nodes.push(node);
+const getAllNodes = (grid: NodeType[][]): NodeType[] => {
+    const nodes: NodeType[] = [];
+    for (const col of grid) {
+        for (const node of col) {
+            nodes.push(node);
+        }
     }
-  }
-  return nodes;
+    return nodes;
 };
 
 const updateUnvisitedNeighbors = (node: NodeType, grid: NodeType[][]) => {
-  const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
-  for (const neighbor of unvisitedNeighbors) {
-    neighbor.distance = node.distance + 1;
-    neighbor.previousNode = node;
-  }
+    const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
+    for (const neighbor of unvisitedNeighbors) {
+        neighbor.distance = node.distance + 1;
+        neighbor.previousNode = node;
+    }
 };
 
 export const dijkstra = (
-  grid: NodeType[][],
-  startNode: NodeType,
-  finishNode: NodeType
+    grid: NodeType[][],
+    startNode: NodeType,
+    finishNode: NodeType
 ) => {
-  if (!startNode || !finishNode || startNode === finishNode) {
-    return false;
-  }
+    if (!startNode || !finishNode || startNode === finishNode) return false;
 
-  startNode.distance = 0;
-  const unvisitedNodes = getAllNodes(grid);
-  const visitedNodesInOrder = [];
-  while (!!unvisitedNodes.length) {
-    unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
-    const closestNode = unvisitedNodes.shift();
-    if (closestNode) {
-      if (closestNode.isWall) continue;
-      if (closestNode.distance === Infinity) return visitedNodesInOrder;
-      if (closestNode === finishNode) return visitedNodesInOrder;
-      closestNode.isVisited = true;
-      visitedNodesInOrder.push(closestNode);
-      updateUnvisitedNeighbors(closestNode, grid);
+    startNode.distance = 0;
+    const unvisitedNodes = getAllNodes(grid);
+    const visitedNodesInOrder: NodeType[] = [];
+
+    while (unvisitedNodes.length) {
+        unvisitedNodes.sort((a, b) => a.distance - b.distance);
+        const closestNode = unvisitedNodes.shift();
+
+        if (!closestNode) continue;
+        if (closestNode.isWall) continue;
+        if (closestNode.distance === Infinity) return visitedNodesInOrder;
+        if (closestNode === finishNode) return visitedNodesInOrder;
+
+        closestNode.isVisited = true;
+        visitedNodesInOrder.push(closestNode);
+
+        updateUnvisitedNeighbors(closestNode, grid);
     }
-  }
 };
