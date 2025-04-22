438ded53cbf5804515c63235585e513387536ab9
sava-stanchev <s.stanchev94@gmail.com>
2025-04-22 21:39:08 +0300

    clean up getUnvisitedNeighbors helper func

--- src/HelperFuncs/AlgorithmHelpers.ts
+++ src/HelperFuncs/AlgorithmHelpers.ts
@@ -1,25 +1,32 @@
 import { NodeType } from "../Visualizer/SingleNode/SingleNode";
 
 const getNodesInShortestPathOrder = (finishNode: NodeType | null) => {
-  const nodesInShortestPathOrder = [];
-  let currentNode = finishNode;
-  while (currentNode !== null) {
-    nodesInShortestPathOrder.unshift(currentNode);
-    currentNode = currentNode.previousNode;
-  }
-  return nodesInShortestPathOrder;
+    const nodesInShortestPathOrder = [];
+    let currentNode = finishNode;
+    while (currentNode !== null) {
+        nodesInShortestPathOrder.unshift(currentNode);
+        currentNode = currentNode.previousNode;
+    }
+    return nodesInShortestPathOrder;
 };
 
-const getUnvisitedNeighbors = (node: NodeType, grid: NodeType[][]) => {
-  const neighbors = [];
-  const { col, row } = node;
-  if (row !== grid[0].length - 1) neighbors.push(grid[col][row + 1]);
-  if (col !== grid.length - 1) neighbors.push(grid[col + 1][row]);
-  if (row !== 0) neighbors.push(grid[col][row - 1]);
-  if (col !== 0) neighbors.push(grid[col - 1][row]);
-  return neighbors.filter(
-    (neighbor) => !neighbor.isWall && !neighbor.isVisited
-  );
+const getUnvisitedNeighbors = (
+    node: NodeType,
+    grid: NodeType[][]
+): NodeType[] => {
+    const neighbors: NodeType[] = [];
+    const { col, row } = node;
+    const maxCol = grid.length - 1;
+    const maxRow = grid[0].length - 1;
+
+    if (row < maxRow) neighbors.push(grid[col][row + 1]); // Down
+    if (col < maxCol) neighbors.push(grid[col + 1][row]); // Right
+    if (row > 0) neighbors.push(grid[col][row - 1]); // Up
+    if (col > 0) neighbors.push(grid[col - 1][row]); // Left
+
+    return neighbors.filter(
+        (neighbor) => !neighbor.isWall && !neighbor.isVisited
+    );
 };
 
 export { getNodesInShortestPathOrder, getUnvisitedNeighbors };
